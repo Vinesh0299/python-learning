0377331c93e3ac516eaf644a2b8a6ba9077f8b0a
Katewa, Vinesh | Vinesh | RMI <vinesh.katewa@rakuten.com>
2023-01-25 13:08:45 +0900

    Added tests for xmlrpc module

--- web_interactions_python/test/test_ip.py
+++ web_interactions_python/test/test_ip.py
@@ -1,10 +1,14 @@
 """Tests ip_functions module"""
 
 import unittest
+from webinteractions import get_ipv4address_generator, get_ipv6address_generator
 
 class IPTestMethods(unittest.TestCase):
     """Class tests the ip_functions module"""
-    pass
+    
+    def test_ipv4_address(self):
+        generator = get_ipv4address_generator("192.168.0.0/30")
+        self.assertEqual(len(generator), 2)
 
 
 if __name__ == "__main__":
