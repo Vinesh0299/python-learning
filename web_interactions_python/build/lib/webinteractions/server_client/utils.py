1dc64f831cb474a05bb58e8de48c2eefa3ebb652
Katewa, Vinesh | Vinesh | RMI <vinesh.katewa@rakuten.com>
2023-01-25 11:49:57 +0900

    added test cases for tcp socket threaded and synchronous, also modified utils.py to handle invalid requests

--- web_interactions_python/build/lib/webinteractions/server_client/utils.py
+++ web_interactions_python/build/lib/webinteractions/server_client/utils.py
@@ -5,6 +5,8 @@ def get_synchronous_response(request_data, client_address):
     print(request_data)
     print(f"Received data from {client_address}")
 
+    response = bytes("unhandled request", "ascii")
+
     if request_data == "ping":
         print("Ping sent back")
         response = bytes("ping received", "ascii")
@@ -22,6 +24,8 @@ def get_threaded_response(request_data, thread_name, client_address):
     """Function return response data for requests in threaded server"""
     print(request_data)
     print(f"Received data from {client_address}")
+
+    response = bytes("unhandled request", "ascii")
 
     if request_data == "ping":
         print("Ping sent back")
